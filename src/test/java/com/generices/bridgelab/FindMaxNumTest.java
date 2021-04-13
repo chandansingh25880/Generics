29c811d6ab1baa724a32887aa4d7fb82cddb8a5b
Chandan Singh Pawar <79848243+chandansingh25880@users.noreply.github.com>
2021-04-14 00:45:07 +0530

    Update FindMaxNumTest.java

--- src/test/java/com/generices/bridgelab/FindMaxNumTest.java
+++ src/test/java/com/generices/bridgelab/FindMaxNumTest.java
@@ -9,44 +9,23 @@ public class FindMaxNumTest {
    using compareTo method. */
 
     @Test
-    public void whenGivenThreeNumbersAtFirstNumberIsMaxShouldReturnSameNumber() {
+    public void whenGivenThreeNumbersAtFirstNumberIsMaxShouldReturnMaxFirstPosition() {
         Integer[] intArray = {7, 6, 5};
         Integer result = maxNumber.findMaximum(intArray);
         Assertions.assertEquals(Integer.valueOf(7), result);
     }
 
     @Test
-    public void whenGivenThreeNumbersAtSecondNumberIsMaxShouldReturnSameNumber() {
+    public void whenGivenThreeNumbersAtSecondNumberIsMaxShouldReturnMaxSecondPosition() {
         Integer[] intArray = {4, 5, 1};
         Integer result = maxNumber.findMaximum(intArray);
         Assertions.assertEquals(Integer.valueOf(5), result);
     }
 
     @Test
-    public void whenGivenThreeNumbersAtThirdNumberIsMaxShouldReturnSameNumber() {
-        Double[] doubleArray = {5.9, 2.1, 7.6};
-        Double result = maxNumber.findMaximum(doubleArray);
-        Assertions.assertEquals(Double.valueOf(7.6), result);
-    }
-
-    @Test
-    public void whenGivenThreeStringsAndFirstStringIsMaxShouldReturnSameNumber() {
-        String[] stringArray = {"Grapes", "Apple", "Banana"};
-        String result = maxNumber.findMaximum(stringArray);
-        Assertions.assertEquals("Grapes", result);
-    }
-
-    @Test
-    public void whenGivenThreeStringsAndSecondStringIsMaxShouldReturnSameNumber() {
-        String[] stringArray = {"Apple", "Orange", "Banana"};
-        String result = maxNumber.findMaximum(stringArray);
-        Assertions.assertEquals("Orange", result);
-    }
-
-    @Test
-    public void whenGivenThreeStringsAndThreeStringIsMaxShouldReturnSameNumber() {
-        String[] stringArray = {"Apple", "Banana", "Guava"};
-        String result = maxNumber.findMaximum(stringArray );
-        Assertions.assertEquals("Guava", result);
+    public void whenGivenThreeNumbersAtSecondNumberIsMaxShouldReturnMaxThirdPosition() {
+        Integer[] intArray = {4, 5, 6};
+        Integer result = maxNumber.findMaximum(intArray);
+        Assertions.assertEquals(Integer.valueOf(6), result);
     }
 }
